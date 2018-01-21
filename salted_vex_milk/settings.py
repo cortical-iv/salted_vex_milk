850329be58d181d685e1ec90dadccb05335e3aa3
cortical-iv <cortical.iv@gmail.com>
2018-01-20 22:57:28 -0500

    staticfiles_dirs fixing

--- salted_vex_milk/settings.py
+++ salted_vex_milk/settings.py
@@ -158,11 +158,13 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/1.9/howto/static-files/
 STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 STATIC_URL = '/static/'
+logger.debug(f"BASE_DIR: {BASE_DIR}")
 
 # Extra places for collectstatic to find static files.
-STATICFILES_DIRS = (
-    os.path.join(BASE_DIR, 'static'),
-)
+#STATICFILES_DIRS = (
+#    os.path.join(BASE_DIR, 'static'),
+#)
+#logger.debug(f"STATIFILES_DIRS: {STATICFILES_DIRS}")
 
 
 #Heroku settings
